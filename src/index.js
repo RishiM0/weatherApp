b2a40138a3f12464cc6170bc6df0e988f0ead8cf
RishiM0 <rishi.mantri0803@gmail.com>
2023-07-28 00:39:36 -0400

    Hopefully have the two js files, figure out how to merge them with webpack. Then add what's necessary to see if it works

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// add functionality so the form loads first, then you just load the weather data for the selected city
+// this is obviously worse than having the search form and display on the same page so the user
+// can change their city multiple times without refreshing, but this is for your skills
